0b9e9abb59ff180a3a96fba980304b453244c273
Amirhossein <amirhosseinmoradi4@gmail.com>
2025-01-14 19:46:31 +0330

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
   theme: {
     extend: {},
   },
   darkMode: 'class',
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
